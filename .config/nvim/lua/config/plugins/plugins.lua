3e03d0f38249fb6cbccc6182f640322fb566af7e
joao.martins <joao.martins@alviere.com>
2024-06-26 23:23:52 +0100

    chore: update plugins
    - readded fugitive (fugitive git blame is much better)

--- .config/nvim/lua/config/plugins/plugins.lua
+++ .config/nvim/lua/config/plugins/plugins.lua
@@ -5,17 +5,12 @@ local plugins = {"devdocs", "notify", "oil"}
 
 M.plugins = {
   {"nvim-lua/plenary.nvim", lazy = false},
+  {"lewis6991/gitsigns.nvim"},
   {"tpope/vim-dadbod"},
   {"kristijanhusak/vim-dadbod-completion"},
-  -- {"andymass/vim-matchup"},
-  {"theprimeagen/vim-be-good"},
+  {"tpope/vim-fugitive", keys = {{"<leader>gb", "<cmd>Git blame<cr>"}}},
   {"j-hui/fidget.nvim", event = "LspAttach", tag = "legacy", opts = {}},
   {"windwp/nvim-autopairs", opts = {disable_filetype = {"TelescopePrompt"}}},
-  {
-    "lewis6991/gitsigns.nvim",
-    keys = {{"<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>"}},
-    opts = {numhl = true, current_line_blame_opts = {delay = 100}},
-  },
   {
     "stevearc/dressing.nvim",
     opts = {input = {relative = "win", insert_only = false, prefer_width = 0.4}},
