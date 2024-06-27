7bdeace7e10c0cfc1b3d8f3b07a3a7bdf4e4aaef
joao.martins <joao.martins@alviere.com>
2024-06-27 16:03:03 +0100

    style: dapui layout improvements

--- .config/nvim/lua/config/debuggers/init.lua
+++ .config/nvim/lua/config/debuggers/init.lua
@@ -38,7 +38,25 @@ M.plugins = {
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
-      dapui.setup()
+      dapui.setup({
+        layouts = {
+          {
+            elements = {
+              {id = "scopes", size = 0.45},
+              {id = "breakpoints", size = 0.15},
+              {id = "stacks", size = 0.25},
+              {id = "watches", size = 0.15},
+            },
+            position = "left",
+            size = 0.25,
+          },
+          {
+            elements = {{id = "repl", size = 0.5}, {id = "console", size = 0.5}},
+            position = "bottom",
+            size = 0.25,
+          },
+        },
+      })
 
       local function toggle_breakpoint()
         dap.toggle_breakpoint(nil, nil, nil)
